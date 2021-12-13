ccd57dddbc01e6e6789767328493c6bc83ae9e5c
Alex <development@alexgidge.com>
2021-12-13 08:43:40 +0000

    Fixed tile loading

--- Game/World/Tile.js
+++ Game/World/Tile.js
@@ -1,12 +1,12 @@
-class Tile extends WorldTile {
-    constructor(position, tileType) {
-        super(position);
-        this.TileType = tileType;
-    }
-}
-
 class TileType extends NamedRange {//TODO: Extend to custom tile types with custom sounds etc.
     static Null = new TileType("NULL_TILE");
     static Floor = new TileType("FLOOR_TILE");
     static Wall = new TileType("WALL_TILE");
+}
+
+class Tile extends WorldTile {
+    constructor(tileType) {
+        super();
+        this.TileType = tileType;
+    }
 }
