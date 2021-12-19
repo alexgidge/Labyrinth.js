77cab2b1a3f1ffc6c558ada9c1d56d91839e49c9
Alex <development@alexgidge.com>
2021-12-19 19:23:06 +0000

    Game frontend refactor

--- Web/header.js
+++ Web/header.js
@@ -1,4 +1,5 @@
 $(function () {
+    $('#divPageHeader').html('<button id="btnHome">Back home</button>');
     $('#btnHome').click(function (evt) {
         window.location.href = '../index.html';
     });
--- index.js
+++ index.js
@@ -2,10 +2,28 @@ $(function () {
     $('#btnStart').click(function (evt) {
         window.location.href = 'Web/MapSelect.html';
     });
+    $('#btnHelp').click(function (evt) {
+        window.location.href = 'Web/Help.html';
+    });
     $('#btnSettings').click(function (evt) {
         window.location.href = 'Web/Settings.html';
     });
+    $('#btnAudioSettings').click(function (evt) {
+        window.location.href = 'Web/AudioSettings.html';
+    });
+    $('#btnGraphicsSettings').click(function (evt) {
+        window.location.href = 'Web/GraphicsSettings.html';
+    });
     $('#btnControls').click(function (evt) {
         window.location.href = 'Web/Controls.html';
+    });
+    $('#btnMapManager').click(function (evt) {
+        window.location.href = 'Web/MapManager.html';
+    });
+    $('#btnCode').click(function (evt) {
+        window.location.href = 'https://github.com/alexgidge/labyrinth.js';
+    });
+    $('#btnAbout').click(function (evt) {
+        window.location.href = 'Web/About.html';
     });
 });
